e0d8274d69ca95564e2a0c2203fcd8a929c4964e
Artem Verbat <artem.verbat@gmail.com>
2017-12-27 22:57:05 +0200

    Added mainHeader and mainBanner sections. mainBanner in progress: added testimonials block

--- src/app.js
+++ src/app.js
@@ -1,2 +1,4 @@
-// const $ = window.$ = window.jQuery = require('jquery');
-console.log('test');
+const $ = window.$ = window.jQuery = require('jquery');
+import 'slick-carousel';
+
+import './components/main-banner/main-banner';
---
+++ src/components/main-banner/main-banner.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+
+
+$('.mainBanner__backgrounds').slick({
+  arrows: false,
+  fade: true,
+  asNavFor: '.mainBanner__testimonials'
+});
+
+$('.mainBanner__testimonials').slick({
+  autoplay: true,
+  autoplaySpeed: 5000,
+  asNavFor: '.mainBanner__backgrounds',
+  infinite: true,
+  arrows: false,
+  dots: true
+});
